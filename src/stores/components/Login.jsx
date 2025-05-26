79427ba9cfd6614ba7db2f7d39bb2c826baaa01d
habib <habibbarca2015@gmail.com>
2025-05-26 06:58:21 +0330

    complete login

--- src/stores/components/Login.jsx
+++ src/stores/components/Login.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className=' flex justify-center items-center h-screen'>
+    <div className=' flex justify-center bg-lime-50 items-center h-screen'>
+      <p className='text-center fixed top-20 font-bold text-2xl'>Login Page</p>
 
-    <div className='border-gray-700  justify-center items-center w-1/2 h-1/2 rounded-md shadow-lg
+    <form className='border-gray-700 p-4 flex-col flex bg-white gap-y-2 justify-center items-center w-1/2 h-1/2 rounded-md shadow-lg
      shadow-cyan-500/50' >
-        <p className='text-center'>Specifications</p>
-        <div className='w-1/2 justify-center items-center flex-col flex gap-3'>
-            <input placeholder='Username' type='string'/>
-            <input placeholder='Password' type='password'/>
-        </div>
+      
+        <p className='text-center '>Specifications</p>
+        <div className='w-1/2 justify-center items-center h-3/4 flex-col flex gap-3'>
+        <div>
+          <p>UserName</p>
+            <input placeholder='Enter Username' type='string'/>
 
-         </div>
+        </div>
+        <br/>
+        <div>
+            <p>Password</p>
+            <input placeholder='Enter Password' type='password'/>
+
+        </div>
+        </div>
+        <Link to={"/"}>
+            <button className="bg-lime-500 shadow-lime-500-500/50 relative bottom-6 text-white w-20 mr-5  rounded-md p-1 text-xl">Submit</button>
+           </Link>
+         </form>
     </div>
   )
 }
